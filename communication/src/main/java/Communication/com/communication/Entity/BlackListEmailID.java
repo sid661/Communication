822902bbcce5380fde954b9af9381140c090d9ec
sidharth mishra <mishrasidharth83@GMAIL.COM>
2024-11-07 16:27:37 +0530

    check

--- communication/src/main/java/Communication/com/communication/Entity/BlackListEmailID.java
+++ communication/src/main/java/Communication/com/communication/Entity/BlackListEmailID.java
@@ -1,4 +1,34 @@
 package Communication.com.communication.Entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+@Entity
+@Table(schema = "email", name = "t_email_blacklist")
 public class BlackListEmailID {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "BLACKLIST_SEQUESNCE")
+    @SequenceGenerator(name = "BLACKLIST_SEQUENCE", sequenceName = "email.t_email_blacklist_blacklist_id_seq", allocationSize = 1)
+    @Column(name = "blacklist_id")
+    private Long blackListId;
+    @Column(name = "email_id")
+    private String emailId;
+    @Column(name = "effective_from")
+    private Timestamp effectiveFrom;
+    @Column(name = "black_listed")
+    private String emailIdStatus;
+    @Column(name = "remarks")
+    private String remarks;
+    @Column(name = "company_code")
+    private String companyCode;
+    @Column(name = "created_user")
+    private String createdUser;
+    @Column(name = "created_date")
+    private Timestamp createdDate;
+    @Column(name = "rate_limit")
+    private Long rateCount;
+
 }
