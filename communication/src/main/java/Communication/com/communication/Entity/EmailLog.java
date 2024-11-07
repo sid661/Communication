e2aa56bd89d19844b0262d8a7a817bb9780d309d
sidharth mishra <mishrasidharth83@GMAIL.COM>
2024-11-07 16:58:38 +0530

    EmailLog entity added

--- communication/src/main/java/Communication/com/communication/Entity/EmailLog.java
+++ communication/src/main/java/Communication/com/communication/Entity/EmailLog.java
@@ -1,4 +1,31 @@
 package Communication.com.communication.Entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@Table(name = "t_email_logs", schema = "email")
 public class EmailLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "LOG_SEQUENCE")
+    @SequenceGenerator(name = "LOG_SEQUENCE", sequenceName = "email.t_email_logs_log_id_seq", allocationSize = 1)
+    private Long logId;
+
+    private String referenceId;
+    private String activity;
+    private String method;
+    private String url;
+    private String header;
+    private String request;
+    private String response;
+    @Column(name = "response_code")
+    private Integer responseCode;
+    private String exception;
+    @CreationTimestamp
+    @Column(name = "created_date")
+    private Timestamp createdDate;
 }
